d93d58927562c1c327865c333972dd9b171a8fb5
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-10-19 18:27:37 -0500

    Updates

---
+++ precache-manifest.c2d82ba4bb4d550f3fc99a47a0f03ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff3f6f422cc51c4fd40e08493e65bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a70e136b0251b275ca",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ff0a42d5327524d66521",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "94a70e136b0251b275ca",
+    "url": "/static/js/2.a6bb69e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a6bb69e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0a42d5327524d66521",
+    "url": "/static/js/main.848018d6.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
