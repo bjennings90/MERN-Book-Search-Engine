16cad0ecaaf7fb49fe08d6b171d9f0cd789fe89b
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-10-19 18:59:37 -0500

    updated package.json

---
+++ client/build/precache-manifest.4ae83e349777b2ed771d42806d998169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad42cdcd0862e04c8711a4e2ff82a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc565e4a162089fd53ae",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3e83db00503fc00251ff",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "bc565e4a162089fd53ae",
+    "url": "/static/js/2.5cbab195.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5cbab195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e83db00503fc00251ff",
+    "url": "/static/js/main.4a3ff7ea.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
